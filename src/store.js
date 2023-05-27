5ea7174810af378e8d3075a5516131a2231b4571
Mwxyz2022 <rozhukvlad@gmail.com>
2023-05-27 14:28:09 +0300

    fix

--- src/store.js
+++ src/store.js
@@ -12,5 +12,3 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
-
-;('dsdsdsdad')
